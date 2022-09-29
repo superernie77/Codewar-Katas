3e78eb9e3125fd977bc21f5bcf8e57e74bda4c5c
superernie77 <superernie77@gmail.com>
2022-09-29 10:02:12 +0200

    EndsWith Kata

---
+++ src/katas/StringEndsWith.java
@@ -0,0 +1,14 @@
+package katas;
+
+public class StringEndsWith {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static boolean solution(String str, String ending) {
+		return str.endsWith(ending);
+	  }
+
+}
