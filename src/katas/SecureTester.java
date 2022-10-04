5829418a55a186e2cd59978868ac8ace89bfc7dc
superernie77 <superernie77@gmail.com>
2022-10-04 10:08:35 +0200

    Alphanumeric Kata

---
+++ src/katas/SecureTester.java
@@ -0,0 +1,17 @@
+package katas;
+
+public class SecureTester {
+
+	public static void main(String[] args) {
+		System.out.println(alphanumeric("abcfdAAA"));
+
+	}
+	
+	 public static boolean alphanumeric(String s){
+		 if (s != null && s.length() > 1) {
+			 return s.matches("^[a-zA-Z0-9]*$");
+		 }
+		 return false;
+	}
+
+}
