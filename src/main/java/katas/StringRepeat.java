4a35ccf42b9aaf18ced988ce9857b0d80ad18a39
superernie77 <superernie77@gmail.com>
2023-03-15 10:58:04 +0100

    string repeat kata

--- src/main/java/katas/StringRepeat.java
+++ src/main/java/katas/StringRepeat.java
@@ -1,5 +1,13 @@
 package katas;
 
+import java.util.stream.IntStream;
+
 public class StringRepeat {
+	
+	 public static String repeatStr(final int repeat, final String string) {
+		 	StringBuilder builder = new StringBuilder();
+	        IntStream.range(0, repeat).forEach( i -> builder.append(string) );
+	        return builder.toString();
+	 }
 
 }
