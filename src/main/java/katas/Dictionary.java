22ea625ecd878d1da5255475b1d1c2f1d468acd8
superernie77 <superernie77@gmail.com>
2023-04-22 08:55:22 +0200

    Dictionary Kata

---
+++ src/main/java/katas/Dictionary.java
@@ -0,0 +1,26 @@
+package katas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dictionary {
+	  
+    private Map<String, String> wordMap;
+  
+    public Dictionary(){
+        wordMap = new HashMap<>();
+    }
+
+    public void newEntry(String key, String value){
+        wordMap.put(key, value);
+    }
+
+    public String look(String key){
+    	if (wordMap.containsKey(key)) {
+    		return wordMap.get(key);	
+    	} else {
+    		return "Cant find entry for " + key;
+    	}
+        
+    }
+}
