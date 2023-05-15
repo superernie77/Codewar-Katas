04298eb0780df957cc13b905ccf872b4c4599a2f
Ernesto Elias-Nieland <superernie77@gmail.com>
2023-05-15 09:11:08 +0200

    hello kata

---
+++ src/main/test/katas/HelloLanguagesTest.java
@@ -0,0 +1,25 @@
+package main.test.katas;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import main.java.katas.HelloLanguages;
+
+public class HelloLanguagesTest {
+
+    @Test
+    void testSomething() {
+        assertTrue(HelloLanguages.validateHello("hello"));
+        assertTrue(HelloLanguages.validateHello("ciao bella!"));
+        assertTrue(HelloLanguages.validateHello("salut"));
+        assertTrue(HelloLanguages.validateHello("hallo, salut"));
+        assertTrue(HelloLanguages.validateHello("hombre! Hola!"));
+        assertTrue(HelloLanguages.validateHello("Hallo, wie geht\'s dir?"));
+        assertTrue(HelloLanguages.validateHello("AHOJ!"));
+        assertTrue(HelloLanguages.validateHello("czesc"));
+        assertTrue(HelloLanguages.validateHello("Ahoj"));
+        assertFalse(HelloLanguages.validateHello("meh"));
+    }
+}
